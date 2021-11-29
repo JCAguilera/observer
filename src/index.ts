e83f25809bbf62a253a3a6f59d6155806739fd20
JuanCarlos Aguilera <jcaaguilerat@gmail.com>
2021-11-28 21:47:52 -0300

    Exported all Observer classes in one file

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 import { ObserverApi } from "./observer/ObserverApi";
+import { ObserverOptions } from "./observer/ObserverOptions";
 import { ObserverWrapper } from "./observer/ObserverWrapper";
-export { ObserverWrapper, ObserverApi };
+export { ObserverWrapper, ObserverApi, ObserverOptions };
