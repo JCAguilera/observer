f0707088ef14a9737366eda64a07530b2ec9a38f
JuanCarlos Aguilera <jcaaguilerat@gmail.com>
2021-12-06 22:37:23 -0300

    Moved loadConfig method to utils - also updated tests

--- src/api.ts
+++ src/api.ts
@@ -1,10 +1,7 @@
 #!/usr/bin/env node
-import fs from "fs";
-import crypto from "crypto";
 import { Command } from "commander";
 import { ObserverApi } from "./observer/ObserverApi";
-import { ObserverOptions } from "./observer/ObserverOptions";
-import { Logger } from "./util";
+import { loadConfig } from "./util";
 
 const program = new Command();
 
@@ -25,26 +22,4 @@ if (opts.config) {
     "Please provide the config file path using the --config option"
   );
   process.exit(0);
-}
-
-function loadConfig(path: string): ObserverOptions {
-  let config: ObserverOptions;
-  try {
-    config = JSON.parse(fs.readFileSync(path, "utf-8"));
-  } catch {
-    Logger.error("Could not load the config file, creating example file...");
-    config = {
-      servers: [],
-      apiKey: crypto.randomBytes(20).toString("hex"),
-      port: 3000,
-    };
-    fs.writeFileSync(path, JSON.stringify(config));
-    Logger.log(
-      "Generated config file! Edit it and restart the wrapper to apply changes."
-    );
-    // Display API key
-    console.log("\nYour auto-generated API key is: " + config.apiKey);
-    console.log("DO NOT SHARE IT\n");
-  }
-  return config;
 }
--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,6 @@
+import fs from "fs";
+import crypto from "crypto";
+import { ObserverOptions } from "./observer/ObserverOptions";
 import dayjs from "dayjs";
 
 export class Logger {
@@ -14,4 +17,26 @@ export class Logger {
       ...optionalParams
     );
   }
+}
+
+export function loadConfig(path: string): ObserverOptions {
+  let config: ObserverOptions;
+  try {
+    config = JSON.parse(fs.readFileSync(path, "utf-8"));
+  } catch {
+    Logger.error("Could not load the config file, creating example file...");
+    config = {
+      servers: [],
+      apiKey: crypto.randomBytes(20).toString("hex"),
+      port: 3000,
+    };
+    fs.writeFileSync(path, JSON.stringify(config));
+    Logger.log(
+      "Generated config file! Edit it and restart the wrapper to apply changes."
+    );
+    // Display API key
+    console.log("\nYour auto-generated API key is: " + config.apiKey);
+    console.log("DO NOT SHARE IT\n");
+  }
+  return config;
 }
